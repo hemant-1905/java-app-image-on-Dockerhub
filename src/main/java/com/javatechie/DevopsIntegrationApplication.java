c0a72bce4fb6c3efcc7a0b215cd9dce7c5b4a5d5
hemant-1905 <hemaant07@gmail.com>
2023-06-25 03:06:57 +0530

    Changed message

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hemant welcomes you";
+		return "Hemant welcomes you all";
 	}
 
 	public static void main(String[] args) {
