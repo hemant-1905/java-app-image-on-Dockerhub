a769c16fe653ed49a35bbeb789215d7bfd59a1f9
hemant-1905 <hemaant07@gmail.com>
2023-06-25 02:56:40 +0530

    Changed Websites home page message

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Hemant welcomes you";
 	}
 
 	public static void main(String[] args) {
